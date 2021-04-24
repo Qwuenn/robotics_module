9a65dbe6bccb8e8d5bc04ace0335a710985931d1
Kim <redwoodmc@gmail.com>
2021-04-24 13:14:59 +0800

    refactor: Changed index layout to master.blade.php

---
+++ source/_layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ $page->language ?? 'en' }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="{{ $page->description }}">
+    <link rel="canonical" href="{{ $page->getUrl() }}">
+    <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+    <link rel="icon" href="/favicon.svg">
+    <title>{{ $page->title }}</title>
+</head>
+<body class="text-gray-900 font-sans antialiased">
+    @yield('body')
+    <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
+</body>
+</html>

0c83d9babae2968af59ef1e4075cda136eab6131
Kim <redwoodmc@gmail.com>
2021-04-24 20:00:53 +0800

    style: Centered footer text

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="mt-auto py-4 bg-white">
     <div class="d-flex justify-content-center">
-        <span>
+        <span class="text-center">
             &copy; Erovoutika International Corporation {{ date('Y') }}. All Rights Reserved.
         </span>
     </div>

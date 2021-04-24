97679fe13a4f00a5c230e8e9c6c41d7d89ecd0c5
Kim <redwoodmc@gmail.com>
2021-04-24 23:50:15 +0800

    style: Enhanced footer layout and design
    Enlarged font size to 1.25 rem and added top border

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,6 +1,6 @@
-<footer class="mt-auto py-4 bg-white">
+<footer class="mt-auto py-4 bg-white border-top">
     <div class="d-flex justify-content-center">
-        <span class="text-center">
+        <span class="text-center fs-5">
             &copy; Erovoutika International Corporation {{ date('Y') }}. All Rights Reserved.
         </span>
     </div>

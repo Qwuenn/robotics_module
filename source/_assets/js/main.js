4a02d7b6e77c47dbb6fdd38fa2434586efa0ee83
Kim <redwoodmc@gmail.com>
2021-04-24 20:28:47 +0800

    feat: Initialized overlayscrollbars on body tag

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,2 +1,9 @@
 import 'bootstrap/dist/js/bootstrap.bundle';
 import 'overlayscrollbars';
+
+// Initialize OverlayScrollbars on body element
+document.addEventListener("DOMContentLoaded", function() {
+    //The 1st argument are the elements to which the plugin shall be initialized
+    //The 2nd argument has to be at least a empty object or a object with your desired options
+    OverlayScrollbars(document.querySelectorAll('body'), { });
+});

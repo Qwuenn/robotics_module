daa37cfc201d099dd3671c9ce7ef15f667d99248
Kim <redwoodmc@gmail.com>
2021-04-26 20:18:26 +0800

    refactor: Used svg global helper to embed svg's

--- source/_partials/header.blade.php
+++ source/_partials/header.blade.php
@@ -1,15 +1,13 @@
 <header class="navbar navbar-expand-sm bg-white position-relative shadow-sm py-0 px-md-5">
     <div class="container-xxl">
         <a class="navbar-brand" href="/">
-            <img src="/assets/img/brand.svg" alt="Erovoutika" style="height:76px">
+            {!! $page->svg('brand.svg', ['css' => ['height' => '76px']]) !!}
         </a>
         <form class="d-flex navbar-search">
             <div class="input-group">
                 <input class="form-control" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-primary" type="submit">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                    </svg>
+                    {!! $page->svg('magnifying-glass.svg') !!}
                 </button>
             </div>
         </form>
--- source/index.blade.php
+++ source/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container p-5">
         <div class="row">
             <div class="col-md-6 order-md-2 my-4 text-center">
-                <img src="/assets/img/robot.svg" alt="robot" style="height: 400px"/>
+                {!! $page->svg('robot.svg', ['css' => ['height' => '400px']]) !!}
             </div>
             <div class="col-md-6 order-md-1 py-5 my-auto">
                 <h1 class="fw-bold">{{ $page->title }}</h1>

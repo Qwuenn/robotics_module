94cc48d49452c618df74937e34761ebff0cc022e
Kim <redwoodmc@gmail.com>
2021-04-27 15:40:30 +0800

    feat: Stack styles and scripts in master layout

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="{{ $page->description }}">
     <link rel="canonical" href="{{ $page->getUrl() }}">
-    <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     <link rel="icon" href="/favicon.svg">
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
     <title>{{ $page->title }}</title>
+    <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+    @stack('styles')
 </head>
 <body class="antialiased">
     <div class="d-flex flex-column min-vh-100 bg-light">
@@ -20,5 +21,6 @@
         @include('_partials.footer')
     </div>
     <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
+    @stack('scripts')
 </body>
 </html>

76d8227b8ad43124948dc9fd9cb3dcd487a7c3c7
Kim <redwoodmc@gmail.com>
2021-05-02 14:04:12 +0800

    refactor: Updated svg helper to exclude .svg on file name
    The developer could write any of the following:
    {!! $page->svg('vector.svg') !!} or
    {!! $page->svg('vector') !!}

--- config.php
+++ config.php
@@ -9,13 +9,14 @@ return [
     'description' => 'Website description.',
     'collections' => [],
 
+    // Helpers
     'selected' => function ($page, $section) {
         return Str::contains($page->getPath(), $section) ? 'selected' : '';
     },
 
     'svg' => function($page, $file, $properties = []) {
         $svg = new \DOMDocument();
-        $svg->load("./source/_assets/svg/". $file);
+        $svg->load("./source/_assets/svg/". $file .(str_contains($file, '.svg') ? '' : '.svg'));
 
         if (!is_null($properties)) {
             foreach ($properties as $key => $value) {

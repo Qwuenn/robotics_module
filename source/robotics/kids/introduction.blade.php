433e41262a2756391a2cfa635a68825d238be0f5
Kim <redwoodmc@gmail.com>
2021-05-07 22:28:02 +0800

    refactor: Changed markdown to blade

---
+++ source/robotics/kids/introduction.blade.php
@@ -0,0 +1,138 @@
+@extends('_layouts.module')
+
+@section('introduction')
+    <h1 id="introduction"><b>Introduction</b></h1>
+    <h5>Discusses the concept of Robotics that can be assembled by kids with no computer programming required.</h5>
+    <p>&nbsp</p>
+@endsection
+
+@section('table_of_contents')
+    <li>
+        <a href="#introduction">Introduction</a>
+    </li>
+    <li>
+        <a href="#what-is-robotics">What is Robotics?</a>
+    </li>
+    <li>
+        <a href="#types-of-robots">Types of Robots</a>
+        <ul>
+            <li><a href="#pre-programmed-robots">Pre-programmed Robots</a></li>
+            <li><a href="#humanoid-robots">Humanoid Robots</a></li>
+            <li><a href="#autonomous-robots">Autonomous Robots</a></li>
+            <li><a href="#teleoperated-robots">Teleoperated Robots</a></li>
+            <li><a href="#augmenting-robots">Augmenting Robots</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="#uses-of-robots">Uses of Robots</a>
+        <ul>
+            <li><a href="#manufacturing">Manufacturing</a></li>
+            <li><a href="#logistics">Logistics</a></li>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#travel">Travel</a></li>
+            <li><a href="#healthcare">Healthcare</a></li>
+        </ul>
+    </li>
+@endSection
+
+@section('content')
+    <h3 id="what-is-robotics">What is Robotics?</h3>
+    <p>
+        Robotics is a discipline that combines computer science and engineering. It is the study of how to plan, create,
+        operate, and use robots. Its objective is to develop robots that can assist and help humans in making their lives
+        easier.
+    </p>
+    <p>
+        Robotics is the study of creating robots that can take the place of humans and mimic their behavior. It can be used
+        in a variety of circumstances and for a variety of uses, but often are currently used in hazardous environments,
+        production facilities, or in situations where humans are unable to survive. It is also often designed to mimic
+        humans. This is said to assist of a robot's acceptance in such replicative actions that are normally done by humans.
+        Driving, carrying, speaking, cognition, and some other human behaviour are all performed by these robots.
+    </p>
+
+    <p>&nbsp</p>
+
+    <h3 id="types-of-robots"><b>Types of Robots</b></h3>
+    <p>
+        Robots come in all shape and sizes to efficiently do their task, it depends on their design. Generally, there are
+        five types of robots:
+    </p>
+
+    <h6 id="pre-programmed-robots"><b>Pre-programmed Robots</b></h6>
+    <p>
+        Pre-programmed robots perform basic, monotonous tasks in a regulated environment. The arm performs one purpose — welding
+        a door on, inserting a certain component into the motor, etc. — and its role is to do it longer, quicker, and more
+        reliably than a person.
+    </p>
+
+    <h6 id="humanoid-robots"><b>Humanoid Robots</b></h6>
+    <p>
+        Humanoid robots are machines that resemble and/or imitate human behavior. These robots often perform human-like tasks
+        (such as running, jumping, and holding objects) and are often programmed to resemble humans, including human faces and
+        gestures.
+    </p>
+
+    <h6 id="autonomous-robots"><b>Autonomous Robots</b></h6>
+    <p>
+        Human operators are not needed for autonomous robots to work. This robots are typically designed to perform tasks in
+        open environments without the need for human supervision. They are very special because they use sensors to sense their
+        surroundings and then use decision-making systems (usually a computer) to take the best next action based on their data
+        and goal.
+    </p>
+
+    <h6 id="teleoperated-robots"><b>Teleoperated Robots</b></h6>
+    <p>
+        Teleoperated robots are semi-autonomous bots that use a wireless network to enable remote human control. These robots
+        are typically used in harsh geographical environments, temperatures, and circumstances.
+    </p>
+
+    <h6 id="augmenting-robots"><b>Augmenting Robots</b></h6>
+    <p>
+        Augmenting robotics either develop on actual human skills or supplement those that have been disabled. The world of
+        robotics for human augmentation is one in which science fiction could become reality quite quickly, with bots capable of
+        redefining civilization and making humans faster and stronger.
+    </p>
+
+    <p>&nbsp</p>
+
+    <h3 id="uses-of-robots"><b>Uses of Robots</b></h3>
+    <p>
+        Robots have a wider range of applications, making them an ideal technology for the future. We will now see robotics
+        almost everywhere. We'll see them in hospitals, hotels, and also on the roads.
+    </p>
+
+    <h6 id="manufacturing"><b>Manufacturing</b></h6>
+    <p>
+        The manufacturing industry is most definitely the oldest and most well-known use of robotics. These robots and co-bots
+        (bots that operate alongside humans) test and assemble objects such as vehicles and industrial machinery. More than
+        three million industrial robots are currently in use, according to reports.
+    </p>
+
+    <h6 id="logistics"><b>Logistics</b></h6>
+    <p>
+        Shipping, processing, and quality assurance robotics are increasingly becoming standard equipment for most retailers and
+        logistics firms. Since we now expect our shipments to arrive at lightning speed, logistics firms use robotics in
+        warehouses and on the road to help improve time efficiency.
+    </p>
+
+    <h6 id="home"><b>Home</b></h6>
+    <p>
+        Robots can also be seen in our homes, assisting with our duties, reminding us of our responsibilities, and even
+        entertaining our children. A well-known example of this is the vacuum cleaner. It has progressed to the point that it
+        can do everything from mowing the grass to washing pools on its own.
+    </p>
+
+    <h6 id="travel"><b>Travel</b></h6>
+    <p>
+        Self-driving vehicles are no longer an imagination. Self-driving cars are taking the world by surprise, thanks to a
+        combination of computer analysis and robotics. Companies are now designing self-driving cars that do not require humans
+        to operate.
+    </p>
+
+    <h6 id="healthcare"><b>Healthcare</b></h6>
+    <p>
+        Robots have made a huge progress in the medical industry. These mechanical inventions are used in almost every
+        healthcare industry, from robot-assisted surgery to physical therapy bots that assist humans in recovering from
+        injuries.
+    </p>
+@endsection

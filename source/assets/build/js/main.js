8613f951fa0a61269a06b6889219bdb1264a9f04
Kim <redwoodmc@gmail.com>
2021-05-08 20:22:35 +0800

    refactor: Created common vendor css file

--- source/assets/build/js/main.js
+++ source/assets/build/js/main.js
@@ -6769,6 +6769,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
 /***/ }),
 
+/***/ "./source/_assets/sass/main.scss":
+/*!***************************************!*\
+  !*** ./source/_assets/sass/main.scss ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
 /***/ "./source/_assets/sass/index.scss":
 /*!****************************************!*\
   !*** ./source/_assets/sass/index.scss ***!
@@ -13573,6 +13586,7 @@ var __WEBPACK_AMD_DEFINE_RESULT__;/*!
 /******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
 /******/ 		var installedChunks = {
 /******/ 			"/js/main": 0,
+/******/ 			"css/main": 0,
 /******/ 			"css/module": 0,
 /******/ 			"css/index": 0
 /******/ 		};
@@ -13622,9 +13636,10 @@ var __WEBPACK_AMD_DEFINE_RESULT__;/*!
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["css/module","css/index"], () => (__webpack_require__("./source/_assets/js/main.js")))
-/******/ 	__webpack_require__.O(undefined, ["css/module","css/index"], () => (__webpack_require__("./source/_assets/sass/index.scss")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/module","css/index"], () => (__webpack_require__("./source/_assets/sass/module.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/main","css/module","css/index"], () => (__webpack_require__("./source/_assets/js/main.js")))
+/******/ 	__webpack_require__.O(undefined, ["css/main","css/module","css/index"], () => (__webpack_require__("./source/_assets/sass/main.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/main","css/module","css/index"], () => (__webpack_require__("./source/_assets/sass/index.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/main","css/module","css/index"], () => (__webpack_require__("./source/_assets/sass/module.scss")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
--- webpack.mix.js
+++ webpack.mix.js
@@ -6,6 +6,7 @@ mix.setPublicPath('source/assets/build');
 
 mix.jigsaw()
     .js('source/_assets/js/main.js', 'js')
+    .sass('source/_assets/sass/main.scss', 'css')
     .sass('source/_assets/sass/index.scss', 'css')
     .sass('source/_assets/sass/module.scss', 'css')
     .options({

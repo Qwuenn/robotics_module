061c981cb1e5a30a1e63d8bccad66201f1e630e4
Kim <redwoodmc@gmail.com>
2021-05-10 20:31:08 +0800

    feat: Improved module layout

--- source/_layouts/module.blade.php
+++ source/_layouts/module.blade.php
@@ -5,7 +5,7 @@
 @endpush
 
 @section('body')
-    <header class="navbar navbar-expand-lg navbar-light bg-white position-relative p-lg-0 p-2 justify-content-end">
+    <header class="subnavbar navbar navbar-expand-lg navbar-light bg-white position-relative p-lg-0 p-2 justify-content-end">
         <button class="navbar-toggler border-none collapsed" 
             type="button" 
             data-bs-toggle="collapse" 
@@ -16,32 +16,30 @@
             <span class="navbar-toggler-icon"></span>
         </button>
     </header>
-    <section class="container-xxl container-fluid">
-        <div class="row">
-            {{-- Sidebar --}}
-            <div class="col-lg-2 ps-lg-3 pt-lg-5">
-                @include('_partials.sidebar')
+    <div class="container-xxl layout px-0">
+        {{-- Sidebar --}}
+        @include('_partials.sidebar')
+        
+        <main class="mx-0 p-lg-5 py-5 px-3 shadow-sm">
+
+            <div class="intro pe-lg-5">
+                @yield('introduction')
             </div>
-            <div class="col-lg-10 p-lg-5 bg-white shadow-sm">
-                <div class="row">
-                    {{-- Main Content --}}
-                    <div class="col-lg-9 order-lg-1 order-2">
-                        @yield('introduction')
-                        @yield('content')
-                    </div>
-                    {{-- Table of Contents --}}
-                    <div class="col-lg-3 order-lg-3 order-1  table-of-contents">
-                        <div class="d-flex align-items-center pb-1 mb-1 mt-lg-0 mt-5 border-bottom disabled">
-                            <span class="fw-semibold">On this page</span>
-                        </div>
-                        <nav id="table-of-contents">
-                            <ul>
-                                @yield('table_of_contents')
-                            </ul>
-                        </nav>
-                    </div>
+
+            <div class="toc table-of-contents pb-4">
+                <div class="d-flex align-items-center pb-1 mb-1 border-bottom disabled">
+                    <span class="fw-semibold">On this page</span>
                 </div>
+                <nav id="table-of-contents">
+                    <ul>
+                        @yield('table_of_contents')
+                    </ul>
+                </nav>
             </div>
-        </div>
-    </section>
+
+            <div class="content pe-lg-5">
+                @yield('content')
+            </div>
+        </main>
+    </div>
 @endSection
--- source/_partials/sidebar.blade.php
+++ source/_partials/sidebar.blade.php
@@ -1,37 +1,40 @@
-<nav id="sidebar" class="d-lg-block sidebar collapse">
-    <ul class="nav flex-column">
-        <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="/module/introduction">
-                Introduction
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" 
-                data-bs-toggle="collapse" 
-                href="#robotics-kit-collapse" 
-                role="button" 
-                aria-expanded="false" 
-                aria-controls="collapseExample">
-                Robotics Kit
-            </a>
-            <div class="collapse" id="robotics-kit-collapse">
-                <ul class="btn-toggle-nav list-unstyled">
-                    <li><a href="#">Sub Menu</a></li>
-                    <li><a href="#">Sub Menu</a></li>
-                    <li><a href="#">Sub Menu</a></li>
-                    <li><a href="#">Sub Menu</a></li>
-                </ul>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">
-                Menu
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">
-                Menu
-            </a>
-        </li>
-    </ul>
-</nav>
+<div id="sidebar" class="sidebar d-lg-block collapse ps-lg-3 mt-lg-5 py-lg-0 py-3 px-2">
+    <div class="position-sticky container-xxl">
+        <div class="navbar-brand text-primary mb-2 pt-lg-0 pt-2 pb-1">Robotics for Kids!</div>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="/module/introduction">
+                    Introduction
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" 
+                    data-bs-toggle="collapse" 
+                    href="#robotics-kit-collapse" 
+                    role="button" 
+                    aria-expanded="false" 
+                    aria-controls="collapseExample">
+                    Robotics Kit
+                </a>
+                <div class="collapse" id="robotics-kit-collapse">
+                    <ul class="btn-toggle-nav list-unstyled">
+                        <li><a href="#">Sub Menu</a></li>
+                        <li><a href="#">Sub Menu</a></li>
+                        <li><a href="#">Sub Menu</a></li>
+                        <li><a href="#">Sub Menu</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    Menu
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    Menu
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>

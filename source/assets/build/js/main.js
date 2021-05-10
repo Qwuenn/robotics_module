f4711aa0a247cda8225968d9f7ca215792b13f17
Kim <redwoodmc@gmail.com>
2021-05-11 01:06:10 +0800

    style: Replaced double quotes with single quotes

--- source/assets/build/js/main.js
+++ source/assets/build/js/main.js
@@ -16,7 +16,7 @@ __webpack_require__.r(__webpack_exports__);
 
  // Initialize OverlayScrollbars on body element
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   //The 1st argument are the elements to which the plugin shall be initialized
   //The 2nd argument has to be at least a empty object or a object with your desired options
   OverlayScrollbars(document.querySelectorAll('body'), {});

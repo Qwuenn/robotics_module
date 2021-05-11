c08baea05216d678bd94db4cfe46b9323bed4333
Kim <redwoodmc@gmail.com>
2021-05-11 14:49:41 +0800

    style: Indents and break to newline

--- source/_partials/sidebar.blade.php
+++ source/_partials/sidebar.blade.php
@@ -3,9 +3,7 @@
         <div class="navbar-brand mb-1 pt-lg-0 pt-2 pb-1">Robotics for Kids!</div>
         <ul class="nav flex-column">
             <li class="nav-item {{ $page->selected('/robotics\kids/introduction') }}">
-                <a class="nav-link" href="/robotics/kids/introduction">
-                    Introduction
-                </a>
+                <a class="nav-link" href="/robotics/kids/introduction">Introduction</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" 
@@ -18,10 +16,18 @@
                 </a>
                 <div class="collapse" id="robotics-kit-collapse">
                     <ul class="nav flex-column btn-toggle-nav">
-                        <li class="nav-item"><a class="nav-link" href="#">Sub Menu</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Sub Menu</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Sub Menu</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Sub Menu</a></li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sub Menu</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sub Menu</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sub Menu</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sub Menu</a>
+                        </li>
                     </ul>
                 </div>
             </li>

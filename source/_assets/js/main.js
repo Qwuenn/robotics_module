01e1bcbf8fc7d62cbddf08618aa7bc69955d2ab8
Kim <redwoodmc@gmail.com>
2021-05-19 17:16:10 +0800

    feat: Initialize OverlayScrollbars on .modal-body

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/js/bootstrap.bundle';
 import 'overlayscrollbars';
+import OverlayScrollbars from 'overlayscrollbars';
 
 // Initialize OverlayScrollbars on body element
 document.addEventListener('DOMContentLoaded', function() {
     //The 1st argument are the elements to which the plugin shall be initialized
     //The 2nd argument has to be at least a empty object or a object with your desired options
     OverlayScrollbars(document.querySelectorAll('body'), { });
+    OverlayScrollbars(document.querySelectorAll('.modal-body'), { });
 });

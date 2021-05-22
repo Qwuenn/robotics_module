60337cc869ddad1d16d201bfde8f9eb8ba2e0dea
Kim <redwoodmc@gmail.com>
2021-05-22 18:34:42 +0800

    fix: Removed children of types/uses of robots toc

--- source/categories/kids/introduction.blade.php
+++ source/categories/kids/introduction.blade.php
@@ -9,26 +9,8 @@
 @section('table_of_contents')
     <li><a href="#introduction">Introduction</a></li>
     <li><a href="#what-is-robotics">What is Robotics?</a></li>
-    <li>
-        <a href="#types-of-robots">Types of Robots</a>
-        <ul>
-            <li><a href="#pre-programmed-robots">Pre-programmed Robots</a></li>
-            <li><a href="#humanoid-robots">Humanoid Robots</a></li>
-            <li><a href="#autonomous-robots">Autonomous Robots</a></li>
-            <li><a href="#teleoperated-robots">Teleoperated Robots</a></li>
-            <li><a href="#augmenting-robots">Augmenting Robots</a></li>
-        </ul>
-    </li>
-    <li>
-        <a href="#uses-of-robots">Uses of Robots</a>
-        <ul>
-            <li><a href="#manufacturing">Manufacturing</a></li>
-            <li><a href="#logistics">Logistics</a></li>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#travel">Travel</a></li>
-            <li><a href="#healthcare">Healthcare</a></li>
-        </ul>
-    </li>
+    <li><a href="#types-of-robots">Types of Robots</a></li>
+    <li><a href="#uses-of-robots">Uses of Robots</a></li>
 @endSection
 
 @section('content')
@@ -54,21 +36,21 @@
         five types of robots:
     </p>
 
-    <h6 id="pre-programmed-robots">Pre-programmed Robots</h6>
+    <h6>Pre-programmed Robots</h6>
     <p>
         Pre-programmed robots perform basic, monotonous tasks in a regulated environment. The arm performs one purpose — welding
         a door on, inserting a certain component into the motor, etc. — and its role is to do it longer, quicker, and more
         reliably than a person.
     </p>
 
-    <h6 id="humanoid-robots">Humanoid Robots</h6>
+    <h6>Humanoid Robots</h6>
     <p>
         Humanoid robots are machines that resemble and/or imitate human behavior. These robots often perform human-like tasks
         (such as running, jumping, and holding objects) and are often programmed to resemble humans, including human faces and
         gestures.
     </p>
 
-    <h6 id="autonomous-robots">Autonomous Robots</h6>
+    <h6>Autonomous Robots</h6>
     <p>
         Human operators are not needed for autonomous robots to work. This robots are typically designed to perform tasks in
         open environments without the need for human supervision. They are very special because they use sensors to sense their
@@ -76,13 +58,13 @@
         and goal.
     </p>
 
-    <h6 id="teleoperated-robots">Teleoperated Robots</h6>
+    <h6>Teleoperated Robots</h6>
     <p>
         Teleoperated robots are semi-autonomous bots that use a wireless network to enable remote human control. These robots
         are typically used in harsh geographical environments, temperatures, and circumstances.
     </p>
 
-    <h6 id="augmenting-robots">Augmenting Robots</h6>
+    <h6>Augmenting Robots</h6>
     <p>
         Augmenting robotics either develop on actual human skills or supplement those that have been disabled. The world of
         robotics for human augmentation is one in which science fiction could become reality quite quickly, with bots capable of
@@ -97,35 +79,35 @@
         almost everywhere. We'll see them in hospitals, hotels, and also on the roads.
     </p>
 
-    <h6 id="manufacturing">Manufacturing</h6>
+    <h6>Manufacturing</h6>
     <p>
         The manufacturing industry is most definitely the oldest and most well-known use of robotics. These robots and co-bots
         (bots that operate alongside humans) test and assemble objects such as vehicles and industrial machinery. More than
         three million industrial robots are currently in use, according to reports.
     </p>
 
-    <h6 id="logistics">Logistics</h6>
+    <h6>Logistics</h6>
     <p>
         Shipping, processing, and quality assurance robotics are increasingly becoming standard equipment for most retailers and
         logistics firms. Since we now expect our shipments to arrive at lightning speed, logistics firms use robotics in
         warehouses and on the road to help improve time efficiency.
     </p>
 
-    <h6 id="home">Home</h6>
+    <h6>Home</h6>
     <p>
         Robots can also be seen in our homes, assisting with our duties, reminding us of our responsibilities, and even
         entertaining our children. A well-known example of this is the vacuum cleaner. It has progressed to the point that it
         can do everything from mowing the grass to washing pools on its own.
     </p>
 
-    <h6 id="travel">Travel</h6>
+    <h6>Travel</h6>
     <p>
         Self-driving vehicles are no longer an imagination. Self-driving cars are taking the world by surprise, thanks to a
         combination of computer analysis and robotics. Companies are now designing self-driving cars that do not require humans
         to operate.
     </p>
 
-    <h6 id="healthcare">Healthcare</h6>
+    <h6>Healthcare</h6>
     <p>
         Robots have made a huge progress in the medical industry. These mechanical inventions are used in almost every
         healthcare industry, from robot-assisted surgery to physical therapy bots that assist humans in recovering from

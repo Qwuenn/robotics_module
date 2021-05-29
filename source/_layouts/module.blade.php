533adbde8b5c01b4b50f4397bd95d3e12c50dd1d
Kim <redwoodmc@gmail.com>
2021-05-29 18:50:09 +0800

    refactor: Updated toc bottom spacing

--- source/_layouts/module.blade.php
+++ source/_layouts/module.blade.php
@@ -30,7 +30,7 @@
                 @yield('introduction')
             </div>
 
-            <div id="table-of-contents" class="toc table-of-contents pb-4">
+            <div id="table-of-contents" class="toc table-of-contents mb-5">
                 <div class="d-flex align-items-center pb-1 mb-1 border-bottom disabled">
                     <span class="fw-semibold">On this page</span>
                 </div>

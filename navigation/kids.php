fb74ed70ebe7bec4587a1912d91253dfdecfdb96
Kim <redwoodmc@gmail.com>
2021-05-29 17:44:02 +0800

    refactor: Moved all activities into ./activities

--- navigation/kids.php
+++ navigation/kids.php
@@ -4,10 +4,10 @@ return [
     'Introduction' => 'categories\kids/introduction',
     'Robot Car Kit' => 'categories\kids/robot-car-kit',
     'Activities' => [
-        '1. Line Following Robot' => 'categories\kids/activity-1',
-        '2. Obstacle Avoidance Robot' => 'categories\kids/activity-2',
-        '3. Light Tracker Robot' => 'categories\kids/activity-3',
-        '4. Motion Tracker Robot' => 'categories\kids/activity-4',
-        '5. Rover Robot Car' => 'categories\kids/activity-5',
+        '1. Line Following Robot' => 'categories\kids\activities/1',
+        '2. Obstacle Avoidance Robot' => 'categories\kids\activities/2',
+        '3. Light Tracker Robot' => 'categories\kids\activities/3',
+        '4. Motion Tracker Robot' => 'categories\kids\activities/4',
+        '5. Rover Robot Car' => 'categories\kids\activities/5',
     ]
 ];

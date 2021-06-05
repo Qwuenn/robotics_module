a4e5aee84a87515ef643eb02bd8a290f292f1420
Kim <redwoodmc@gmail.com>
2021-06-05 23:06:54 +0800

    docs: Removed comments

--- source/_toc/toc.blade.php
+++ source/_toc/toc.blade.php
@@ -6,36 +6,3 @@
         @include('_toc.list')
     </nav>
 </div>
-
-{{-- 
-    Can be
-
-		@include('_toc.toc', ['items' => [
-            'List 1',
-            'List 2',
-            'List 3',
-            'List 4' => [
-                'Child 1',
-                'Child 2',
-                'Child 3'
-            ]
-		]])
-
-    or
-
-		@include('_toc.toc', ['items' => [
-            'List 1' => 'list-1',
-            'List 2' => 'list-2',
-            'List 3' => 'list-3',
-            'List 4' => [
-                'section' => 'list-4',
-                'children' => [
-                    'Child 1' => 'child-1',
-                    'Child 2' => 'child-2',
-                    'Child 3' => 'child-3'
-                ]
-            ]
-		]])
-    
-    depending on your preference.
---}}

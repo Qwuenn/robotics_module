016fc46d9c78f147e35baf4a46dd0fe0795b9d37
Kim <redwoodmc@gmail.com>
2021-06-06 14:28:26 +0800

    feat: Added og and twitter meta tags for social media

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,13 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="{{ $page->description }}">
     <meta name="developer" content="{{ $page->developer }}">
+
+    <meta property="og:title" content="{{ $page->title }}"/>
+    <meta property="og:description" content="{{ $page->description }}"/>
+    <meta property="og:url" content="{{ $page->getUrl() }}"/>
+    <meta property="og:image" content="{{ $page->link('assets/img/robot-hero-vector.svg') }}"/>
+    <meta property="og:type" content="website"/>
+
+    <meta name="twitter:title" content="{{ $page->title }}">
+    <meta name="twitter:description" content="{{ $page->description }}">
+    <meta name="twitter:image" content="{{ $page->link('assets/img/robot-hero-vector.svg') }}">
+    <meta name="twitter:image:alt" content="{{ $page->title }}">
+    <meta name="twitter:card" content="summary_large_image">
+
     <link rel="canonical" href="{{ $page->getUrl() }}">
     <link rel="icon" href="{{ $page->link('favicon.svg') }}">
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
-    <title>{{ $page->title }}</title>
     <link rel="stylesheet" href="{{ $page->link(mix('css/main.css', 'assets/build')) }}">
+
+    <title>{{ $page->title }}</title>
+    
     @stack('styles')
 </head>
 <body class="antialiased">

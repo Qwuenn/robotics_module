24735df7ba712d9b09cb76db7f2a47d78e71c9ce
Kim <redwoodmc@gmail.com>
2021-06-06 12:18:57 +0800

    refactor: Moved kids module 1 to /kids/module/1

--- navigation/kids.php
+++ navigation/kids.php
@@ -1,16 +1,21 @@
 <?php
 
 return [
-    'Introduction' => 'categories\kids/introduction',
-    'Robot Car Kit' => 'categories\kids/robot-car-kit',
-    'Activities' => [
+    'Module 1 - Robot Car' => [
         'collapsible' => true,
         'children' => [
-            '1. Line Following Robot' => 'categories\kids\activities/1',
-            '2. Obstacle Avoidance Robot' => 'categories\kids\activities/2',
-            '3. Light Tracker Robot' => 'categories\kids\activities/3',
-            '4. Motion Tracker Robot' => 'categories\kids\activities/4',
-            '5. Rover Robot Car' => 'categories\kids\activities/5'
+            'Introduction' => 'categories\kids\modules\1/introduction',
+            'Robot Car Kit' => 'categories\kids\modules\1/robot-car-kit',
+            'Activities' => [
+                'collapsible' => true,
+                'children' => [
+                    'Line Following Robot' => 'categories\kids\modules\1\activities/1',
+                    'Obstacle Avoidance Robot' => 'categories\kids\modules\1\activities/2',
+                    'Light Tracker Robot' => 'categories\kids\modules\1\activities/3',
+                    'Motion Tracker Robot' => 'categories\kids\modules\1\activities/4',
+                    'Rover Robot Car' => 'categories\kids\modules\1\activities/5'
+                ]
+            ]
         ]
     ]
 ];

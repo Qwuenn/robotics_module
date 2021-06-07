a37e4a7ca983f9efdb77b741aa32e2b14386aec7
Kim <redwoodmc@gmail.com>
2021-06-07 19:48:53 +0800

    feat: Added image partial

---
+++ source/_partials/image.blade.php
@@ -0,0 +1,8 @@
+<div class="col">
+    <img class="img-fluid rounded-2"
+        src="{{ isset($img) ? $img : '' }}"
+        alt="{{ isset($alt) ? $alt : 'N/A' }}" />
+    @isset($label)
+        <p class="fst-italic mt-2">{{ $label }}</p>
+    @endisset
+</div>

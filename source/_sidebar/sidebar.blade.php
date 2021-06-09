5396471aa03da9f6491a2f42fb0fc40b63687fc8
Kim <redwoodmc@gmail.com>
2021-06-09 21:59:59 +0800

    fix: Sidebar stickiness spacing and height

--- source/_sidebar/sidebar.blade.php
+++ source/_sidebar/sidebar.blade.php
@@ -1,6 +1,6 @@
-<div id="sidebar" class="sidebar d-lg-block collapse">
-    <div class="position-sticky py-lg-0 py-4 px-2 mt-lg-5 mb-lg-5 pb-lg-3">
-        <div class="navbar-brand border-bottom border-primary text-primary fw-semibold mb-1 pt-lg-0 pt-2 pb-1">{{ $title }}</div>
+<div id="sidebar" class="sidebar d-lg-block collapse my-lg-5">
+    <div class="module-nav pt-lg-0 py-4 pb-lg-5 px-lg-2">
+        <div class="navbar-brand border-bottom border-primary text-primary fw-semibold mb-1 pt-lg-0 pb-1">{{ $title }}</div>
         @include('_sidebar.nav')
     </div>
 </div>

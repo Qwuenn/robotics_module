9ed3386900b53c512cbc497af5dafcdec5300d3b
Kim <redwoodmc@gmail.com>
2021-06-09 22:16:24 +0800

    refactor: Change div to nav

--- source/_sidebar/sidebar.blade.php
+++ source/_sidebar/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div id="sidebar" class="sidebar d-lg-block collapse my-lg-5">
-    <div class="module-nav pt-lg-0 py-4 pb-lg-5 px-lg-2">
+    <nav class="module-nav pt-lg-0 py-4 pb-lg-5 px-lg-2">
         <div class="navbar-brand border-bottom border-primary text-primary fw-semibold mb-1 pt-lg-0 pb-1">{{ $title }}</div>
         @include('_sidebar.nav')
-    </div>
+    </nav>
 </div>

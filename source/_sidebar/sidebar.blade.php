0e323f07f003dd061bc1d3b91b198e20264b0e88
Kim <redwoodmc@gmail.com>
2021-06-09 19:42:14 +0800

    fix: Sidebar not scrolling properly to the bottom

--- source/_sidebar/sidebar.blade.php
+++ source/_sidebar/sidebar.blade.php
@@ -1,5 +1,5 @@
-<div id="sidebar" class="sidebar d-lg-block collapse mt-lg-5">
-    <div class="position-sticky px-lg-2 py-lg-0 py-4 px-2">
+<div id="sidebar" class="sidebar d-lg-block collapse">
+    <div class="position-sticky px-lg-2 py-lg-0 py-4 px-2 mt-lg-5 mb-lg-5 pb-lg-3">
         <div class="navbar-brand border-bottom border-primary text-primary fw-semibold mb-1 pt-lg-0 pt-2 pb-1">{{ $title }}</div>
         @include('_sidebar.nav')
     </div>

dd49d8aa4c0a412f6dff1a38a02fa47b6f79eade
Kim <redwoodmc@gmail.com>
2021-06-09 22:23:59 +0800

    refactor: Made TOC equal to sidebar viewport height

--- source/_toc/toc.blade.php
+++ source/_toc/toc.blade.php
@@ -1,4 +1,4 @@
-<div id="table-of-contents" class="toc table-of-contents mb-5">
+<div id="table-of-contents" class="toc table-of-contents">
     <div class="border-bottom pb-1 mb-1 disabled">
         <span class="fw-semibold">On this page</span>
     </div>

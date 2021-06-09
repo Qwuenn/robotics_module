53e0270a0991893929c15ba6ae57a036abf2bb66
Kim <redwoodmc@gmail.com>
2021-06-09 20:35:02 +0800

    refactor: Removed unneccessary classes

--- source/_toc/toc.blade.php
+++ source/_toc/toc.blade.php
@@ -1,5 +1,5 @@
 <div id="table-of-contents" class="toc table-of-contents mb-5">
-    <div class="d-flex align-items-center pb-1 mb-1 border-bottom disabled">
+    <div class="border-bottom pb-1 mb-1 disabled">
         <span class="fw-semibold">On this page</span>
     </div>
     <nav>

d578ddf925f055e76542df487007625a8bdf378a
Kim <redwoodmc@gmail.com>
2021-06-11 14:03:10 +0800

    refactor: Deleted component_card partial of teens

--- source/_partials/teens/component_card.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="col bg-light rounded-5 shadow p-4">
-    <img class="img-fluid mx-auto d-block"
-        alt="{{ $component }}"
-        src="{{ $page->link('assets/build/img/categories/teens/components/'. str_replace(' ', '-', strtolower($component)) .'.png') }}" />
-    <h4 class="mt-2 mb-3">{{ isset($title) ? $title : $component }}</h4>
-    @isset ($description)
-        <p class="text-justify">{{ $description }}</p>
-    @endisset
-</div>

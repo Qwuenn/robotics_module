140048907538053f1d547d168ecb2691185f4637
Kim <redwoodmc@gmail.com>
2021-06-19 18:42:05 +0800

    feat: Created Introduction page of Robotics for Adults

--- source/_partials/component_symbol.blade.php
+++ source/_partials/component_symbol.blade.php
@@ -1,11 +1,11 @@
-<div class="card component-card bg-light">
-    <div id="carousel{{ str_replace(' ', '', (isset($id) ? $id : $component)) }}" 
+<div class="col card component-card bg-light shadow">
+    <div id="carousel{{ str_replace(' ', '', (isset($id) ? $id : $title )) }}" 
         class="carousel slide card-img-top mx-auto" 
         data-bs-ride="carousel" 
         data-bs-interval="false">
         <div class="carousel-indicators">
             <button type="button" 
-                data-bs-target="#carousel{{ str_replace(' ', '', (isset($id) ? $id : $component)) }}" 
+                data-bs-target="#carousel{{ str_replace(' ', '', (isset($id) ? $id : $title )) }}" 
                 data-bs-slide-to="0" 
                 @if ((isset($active) && $active == 0) || !isset($active))
                     class="active component-picture"
@@ -16,7 +16,7 @@
                 >
             </button>
             <button type="button" 
-                data-bs-target="#carousel{{ str_replace(' ', '', (isset($id) ? $id : $component)) }}" 
+                data-bs-target="#carousel{{ str_replace(' ', '', (isset($id) ? $id : $title )) }}" 
                 data-bs-slide-to="1" 
                 @if (isset($active) && $active == 1)
                     class="active component-symbol"
@@ -27,39 +27,39 @@
                 >
             </button>
         </div>
-        <div class="carousel-inner mt-3 mb-5">
+        <div class="carousel-inner w-100 mt-3 mb-5">
             <div class="carousel-item {{ (isset($active) && $active === 0) || !isset($active) ? 'active' : '' }}">
-                <img src="{{ $page->link('assets/build/img/categories/adults/components/p-'. str_replace(' ', '-', strtolower($component)) .'.png') }}" 
-                    alt="Picture of {{ $component }}" 
-                    class="mx-auto d-block" />
+                <img src="{{ $img }}" 
+                    alt="Picture of {{ $title  }}" 
+                    class="img-fluid mx-auto d-block" />
             </div>
             <div class="carousel-item {{ (isset($active) && $active === 1) ? 'active' : '' }}">
-                <img src="{{ $page->link('assets/build/img/categories/adults/components/s-'. str_replace(' ', '-', strtolower($component)) .'.png') }}"
-                    alt="Symbol of {{ $component }}" 
-                    class="mx-auto d-block" />
+                <img src="{{ $symbol }}"
+                    alt="Symbol of {{ $title  }}" 
+                    class="img-fluid mx-auto d-block" />
             </div>
         </div>
         <button class="carousel-control-prev" 
             type="button" 
-            data-bs-target="#carousel{{ str_replace(' ', '', (isset($id) ? $id : $component)) }}" 
+            data-bs-target="#carousel{{ str_replace(' ', '', (isset($id) ? $id : $title)) }}" 
             data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" 
             type="button" 
-            data-bs-target="#carousel{{ str_replace(' ', '', (isset($id) ? $id : $component)) }}" 
+            data-bs-target="#carousel{{ str_replace(' ', '', (isset($id) ? $id : $title)) }}" 
             data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
     </div>
-    <div class="card-body">
-        <h5 class="card-title text-center">
-            {{ isset($title) ? $title : $component}}
-        </h5>
+    <div class="card-body mb-4">
+        <h4 class="card-title">
+            {{ $title }}
+        </h4>
         @isset ($description)
-            <p class="card-text small">{{ $description }}</p>
+            <p class="card-text">{!! $description !!}</p>
         @endisset
     </div>
 </div>
--- source/categories/adults/introduction.blade.php
+++ source/categories/adults/introduction.blade.php
@@ -44,10 +44,10 @@
 
     <div class="container-fluid">
         <div class="row row-cols-1 gy-5">
-            @include('_partials.adults.component_card', [
-                'component' => 'Arduino Uno R3',
+            @include('_partials.component_symbol', [
+                'img' => $page->link('assets/build/img/components/arduino-uno-r3.png'),
+                'symbol' => $page->link('assets/build/img/components/symbols/arduino-uno.png'),
                 'title' => 'Arduino Uno',
-                'id' => 'r1',
                 'description' => '
                     The Arduino Uno is a Microchip ATmega328P-based open-source microcontroller board created by Arduino. The board has a
                     number of digital and analog input/output (I/O) pins that can be used to connect to various expansion boards (shields)
@@ -55,6 +55,90 @@
                     is programmable via a type B USB cable with the Arduino IDE (Integrated Development Environment). It can be powered by a
                     USB cable or an external 9-volt battery, and it accepts voltages ranging from 7 to 20 volts. It is comparable to the
                     Arduino Nano and Leonardo.
+                '
+            ])
+
+            @include('_partials.component_symbol', [
+                'img' => $page->link('assets/build/img/components/sg90-servo-motor.png'),
+                'symbol' => $page->link('assets/build/img/components/symbols/servo-motor.png'),
+                'title' => 'Servo Motor',
+                'description' => '
+                    A servomotor is a rotary or linear actuator capable of precisely controlling angular or linear position, velocity, and
+                    acceleration. It is made up of a suitable motor connected to a position feedback sensor. It also necessitates the use of
+                    a fairly sophisticated controller, which is often a dedicated module designed specifically for use with servomotors.
+                    Servomotors are not a specific type of motor, though the term is frequently used to refer to a motor that can be used in
+                    a closed-loop control system.
+                '
+            ])
+
+            @include('_partials.component_symbol', [
+                'img' => $page->link('assets/build/img/components/potentiometer.png'),
+                'symbol' => $page->link('assets/build/img/components/symbols/potentiometer.png'),
+                'title' => '10K Pot-Variable Resistor',
+                'description' => '
+                    <p>
+                        A potentiometer, also known as a "pot," is a three-terminal mechanically operated rotary analogue device found and
+                        used in a wide range of electrical and electronic circuits. They are passive devices, which mean they don\'t need a
+                        power supply or extra circuitry to perform their basic linear or rotary position function.
+                    </p>
+                    <p>
+                        Variable potentiometers come in a variety of mechanical variations that allow for simple adjustment to control a
+                        voltage, current, or the biasing and gain control of a circuit to achieve a zero condition.
+                    </p>
+                    <p>
+                        The term "potentiometer" is a combination of the words Potential Difference and Metering, and it dates back to the early
+                        days of electronic development. It was thought at the time that adjusting large wirewound resistive coils metered or
+                        measured a fixed amount of potential difference, thereby making it a type of voltage-metering device.
+                    </p>
+                '
+            ])
+
+            @include('_partials.component_symbol', [
+                'img' => $page->link('assets/build/img/components/1.5v-battery.png'),
+                'symbol' => $page->link('assets/build/img/components/symbols/battery.png'),
+                'title' => 'Battery',
+                'description' => '
+                    A battery is a type of power source that consists of one or more electrochemical cells with external connections that
+                    can be used to power electrical devices such as flashlights, mobile phones, and electric cars. When a battery is
+                    supplying electricity, the positive terminal is known as the cathode and the negative terminal is known as the anode.
+                    The negative terminal serves as the source of electrons that will flow to the positive terminal via an external electric
+                    circuit. When a battery is connected to an external electric load, a redox reaction occurs in which high-energy
+                    reactants are converted to lower-energy products, with the free-energy difference being delivered to the external
+                    circuit as electrical energy. Historically, the term "battery" referred to a device made up of multiple cells; however,
+                    the term has come to refer to devices made up of a single cell as well.
+                '
+            ])
+
+            @include('_partials.component_symbol', [
+                'img' => $page->link('assets/build/img/components/connecting-wires.png'),
+                'symbol' => $page->link('assets/build/img/components/symbols/connecting-wires.png'),
+                'title' => 'Connecting Wires',
+                'description' => '
+                    Since electricity requires a medium to travel through, connecting wires allows an electrical current to travel from one
+                    point on a circuit to another. In the case of computers, wires are embedded in circuit boards, carrying electrical
+                    pulses that are decoded as binary signals of zeros and ones. The majority of computer and electronic component wires are
+                    made of copper or aluminum. Copper is both inexpensive and electrically conductive. Although silver has a higher
+                    conductivity, it is much more expensive. The wire in a basic circuit is connected to one terminal of a power source,
+                    such as a battery. It is then linked to a switch, which determines whether the circuit is open or closed. The wire then
+                    connects to the power-drawing device, allowing it to draw electricity and perform its function. Finally, the wire
+                    connects the load to the power source\'s opposing terminal. Before a current can flow through a wire, the circuit must
+                    be closed; that is, there must be no breaks in the path. Electricity cannot easily travel through air, and if it does,
+                    there is a risk of stray current leaking into the environment and damaging or failing to power the appliance.
+                '
+            ])
+
+            @include('_partials.component_card', [
+                'img' => $page->link('assets/build/img/components/breadboard.png'),
+                'title' => 'Breadboard',
+                'description' => '
+                    A breadboard, also known as a protoboard, is a building base for electronic prototyping. Originally, the term referred
+                    to a literal bread board, which was a polished piece of wood used to slice bread. The solderless breadboard (also known
+                    as a plugboard or a terminal array board) was introduced in the 1970s, and the term "breadboard" is now commonly used to
+                    refer to these. The solderless breadboard is reusable because it does not require soldering. This makes it ideal for
+                    making quick prototypes and experimenting with circuit design. As a result, solderless breadboards are popular among
+                    students and in technological education. Breadboards can be used to prototype a wide range of electronic systems, from
+                    simple analog and digital circuits to full-fledged central processing units (CPUs).
+
                 '
             ])
         </div>

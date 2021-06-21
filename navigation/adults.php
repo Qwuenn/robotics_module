bd2f5bab7faed2f60e56d4e1701f09d7b9715033
Kim <redwoodmc@gmail.com>
2021-06-21 20:40:49 +0800

    refactor: Moved Introduction to Mobility into Fire Fighter Robot children

--- navigation/adults.php
+++ navigation/adults.php
@@ -4,10 +4,10 @@ return [
     'Introduction' => 'categories\adults/introduction',
     'Materials' => 'categories\adults\modules\1/materials',
     'Quiz' => 'categories\adults\modules\1/quiz',
-    'Introduction to Mobility' => 'categories\adults\modules\2/introduction-to-mobility',
     'Fire Fighter Robot' => [
         'collapsible' => true,
         'children' => [
+            'Introduction to Mobility' => 'categories\adults\modules\2/introduction-to-mobility',
             'Circuit Diagram' => 'categories\adults\modules\2/circuit-diagram',
             'Design' => 'categories\adults\modules\2/design'
         ]
